9ab53b27e23616edb2d2d592bfabaf857b352b6a
mazanur <almazic91@gmail.com>
2021-01-12 21:00:28 +0300

    [BF] delete unused methods

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -11,12 +11,6 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type Mine struct {
-	//ID           string `json:"_id" bson:"_id"`
-	Name         string `json:"name" bson:"name"`
-	DiamondCount int    `json:"diamond_count" bson:"diamond_count"`
-}
-
 type Transaction struct {
 	ID    primitive.ObjectID `json:"id" bson:"id"`
 	Donor *api.User          `json:"donor" bson:"donor"`
@@ -39,11 +33,6 @@ type Room struct {
 }
 
 type UserRepository interface {
-	UpdateMine(ctx context.Context, mineName string, newCount int) error
-	FindByName(ctx context.Context, mineName string) (*Mine, error)
-	GetAllMines(ctx context.Context) ([]Mine, error)
-	AddDiamondMine(ctx context.Context, m *Mine) (*Mine, error)
-	EmptyMine(ctx context.Context, mineName string) (diamondCount int, err error)
 	UpsertUser(ctx context.Context, u api.User) error
 }
 
@@ -132,60 +121,4 @@ func (r MongoUserRepository) UpsertUser(ctx context.Context, u api.User) error {
 		return err
 	}
 	return nil
-}
-
-func (r MongoUserRepository) FindByName(ctx context.Context, mineName string) (*Mine, error) {
-	res := r.col.FindOne(ctx, bson.D{{"name", bson.D{{"$eq", mineName}}}})
-	if res.Err() != nil {
-		return nil, res.Err()
-	}
-	m := &Mine{}
-	if err := res.Decode(m); err != nil {
-		return nil, err
-	}
-	return m, nil
-}
-
-func (r MongoUserRepository) UpdateMine(ctx context.Context, mineName string, newCount int) error {
-	f := bson.D{{"name", bson.D{{"$eq", mineName}}}}
-	_, err := r.col.UpdateOne(ctx, f, bson.D{{"$set", bson.D{{"diamond_count", newCount}}}})
-	return err
-}
-
-func (r MongoUserRepository) GetAllMines(ctx context.Context) ([]Mine, error) {
-	cur, err := r.col.Find(ctx, bson.D{})
-	if err != nil {
-		return nil, err
-	}
-	var m []Mine
-	err = cur.All(ctx, &m)
-	if err != nil {
-		return nil, err
-	}
-	return m, nil
-}
-
-func (r MongoUserRepository) AddDiamondMine(ctx context.Context, m *Mine) (*Mine, error) {
-	a, err := r.col.InsertOne(ctx, m)
-	id := a.InsertedID
-	res := r.col.FindOne(ctx, bson.D{{"_id", bson.D{{"$eq", id}}}})
-	q := &Mine{}
-	if err1 := res.Decode(q); err != nil {
-		return nil, err1
-	}
-	return q, nil
-}
-
-func (r MongoUserRepository) EmptyMine(ctx context.Context, mineName string) (diamondCount int, err error) {
-	f := bson.D{{"name", bson.D{{"$eq", mineName}}}}
-	res := r.col.FindOneAndDelete(ctx, f)
-	if res.Err() != nil {
-		return 0, res.Err()
-	}
-	m := &Mine{}
-	err = res.Decode(m)
-	if err != nil {
-		return 0, err
-	}
-	return m.DiamondCount, nil
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -5,18 +5,10 @@ import (
 	"github.com/almaznur91/splitty/internal/api"
 	"github.com/almaznur91/splitty/internal/repository"
 	tbapi "github.com/go-telegram-bot-api/telegram-bot-api"
-	"github.com/labstack/echo/v4"
-	"github.com/rs/zerolog/log"
-	"go.mongodb.org/mongo-driver/mongo"
-	"math/rand"
 )
 
 type Service interface {
 	UpsertUser(ctx context.Context, user tbapi.User) error
-	GetDiamonds(ctx context.Context, mineName string) (int, error)
-	GetAllMines(ctx context.Context) ([]repository.Mine, error)
-	AddDiamondMine(ctx context.Context, m *repository.Mine) (*repository.Mine, error)
-	EmptyMine(ctx context.Context, name string) (diamondCount int, err error)
 	CreateRoom(ctx context.Context, user api.User) (*repository.Room, error)
 	JoinToRoom(ctx context.Context, u *api.User, roomId string) error
 	FindById(ctx context.Context, id string) (*repository.Room, error)
@@ -57,44 +49,4 @@ func (rs *RoomService) CreateRoom(ctx context.Context, u api.User) (*repository.
 
 func (us *UserService) UpsertUser(ctx context.Context, u api.User) error {
 	return us.r.UpsertUser(ctx, u)
-}
-
-func (us *UserService) GetDiamonds(ctx context.Context, mineName string) (int, error) {
-	m, err := us.r.FindByName(ctx, mineName)
-	if err == mongo.ErrNoDocuments {
-		return 0, echo.ErrNotFound
-	}
-	if err != nil {
-		return 0, err
-	}
-	log.Debug().Interface("result", m).Msg("found mine ")
-	if m.DiamondCount <= 0 {
-		return us.EmptyMine(ctx, m.Name)
-	}
-	dc := rand.Intn(m.DiamondCount)
-	nc := m.DiamondCount - dc
-	if err := us.r.UpdateMine(ctx, m.Name, nc); err != nil {
-		return 0, err
-	}
-	return dc, nil
-}
-
-func (us *UserService) GetAllMines(ctx context.Context) ([]repository.Mine, error) {
-	r, err := us.r.GetAllMines(ctx)
-	if err == mongo.ErrNoDocuments {
-		return nil, echo.ErrNotFound
-	}
-	return r, err
-}
-
-func (us *UserService) AddDiamondMine(ctx context.Context, m *repository.Mine) (*repository.Mine, error) {
-	return us.r.AddDiamondMine(ctx, m)
-}
-
-func (us *UserService) EmptyMine(ctx context.Context, name string) (diamondCount int, err error) {
-	r, err := us.r.EmptyMine(ctx, name)
-	if err == mongo.ErrNoDocuments {
-		return 0, echo.ErrNotFound
-	}
-	return r, err
 }
