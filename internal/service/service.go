52e43855cab97cb76e8e386725b3567c140d11e8
zagir <znurgaliev@danytech.ru>
2021-01-16 23:08:33 +0300

    [RF] change interfaces

--- internal/service/service.go
+++ internal/service/service.go
@@ -4,42 +4,22 @@ import (
 	"context"
 	"github.com/almaznur91/splitty/internal/api"
 	"github.com/almaznur91/splitty/internal/repository"
-	tbapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-type Service interface {
-	UpsertUser(ctx context.Context, user tbapi.User) error
-	CreateRoom(ctx context.Context, user api.User) (*repository.Room, error)
-	JoinToRoom(ctx context.Context, u *api.User, roomId string) error
-	FindById(ctx context.Context, id string) (*repository.Room, error)
-	FindRoomsByUserId(ctx context.Context, id int) (*[]repository.Room, error)
+func NewUserService(r repository.UserRepository) *UserService {
+	return &UserService{r}
 }
 
-func NewUserService(r repository.UserRepository) *UserService {
-	return &UserService{r: r}
-}
 func NewRoomService(r repository.RoomRepository) *RoomService {
-	return &RoomService{r: r}
+	return &RoomService{r}
 }
 
 type UserService struct {
-	r repository.UserRepository
+	repository.UserRepository
 }
 
 type RoomService struct {
-	r repository.RoomRepository
-}
-
-func (rs *RoomService) FindRoomsByUserId(ctx context.Context, uId int) (*[]repository.Room, error) {
-	return rs.r.FindRoomsByUserId(ctx, uId)
-}
-
-func (rs *RoomService) JoinToRoom(ctx context.Context, u api.User, roomId string) error {
-	return rs.r.JoinToRoom(ctx, u, roomId)
-}
-
-func (rs *RoomService) FindById(ctx context.Context, id string) (*repository.Room, error) {
-	return rs.r.FindById(ctx, id)
+	repository.RoomRepository
 }
 
 func (rs *RoomService) CreateRoom(ctx context.Context, u api.User) (*repository.Room, error) {
@@ -47,11 +27,7 @@ func (rs *RoomService) CreateRoom(ctx context.Context, u api.User) (*repository.
 		Users: &[]api.User{u},
 		Name:  "Тестовая",
 	}
-	rId, err := rs.r.SaveRoom(ctx, r)
+	rId, err := rs.RoomRepository.SaveRoom(ctx, r)
 	r.ID = rId
 	return r, err
-}
-
-func (us *UserService) UpsertUser(ctx context.Context, u api.User) error {
-	return us.r.UpsertUser(ctx, u)
 }
