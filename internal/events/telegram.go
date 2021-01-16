0ee6e7c4b829485a43c70015e5b2670bc081b0a8
zagir <znurgaliev@danytech.ru>
2021-01-16 21:45:00 +0300

    [RF] hard delete all unnecessary code

--- internal/events/telegram.go
+++ internal/events/telegram.go
@@ -5,29 +5,20 @@ import (
 	"encoding/json"
 	"github.com/almaznur91/splitty/internal/api"
 	"github.com/almaznur91/splitty/internal/service"
-	"log"
-	"sync"
-	"time"
-
 	tbapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/pkg/errors"
+	"log"
+	"sync"
 
 	"github.com/almaznur91/splitty/internal/bot"
 )
 
-//go:generate mockery -inpkg -name tbAPI -case snake
-//go:generate mockery -inpkg -name msgLogger -case snake
-
 // TelegramListener listens to tg update, forward to bots and send back responses
 // Not thread safe
 type TelegramListener struct {
-	TbAPI        tbAPI
-	MsgLogger    msgLogger
-	Bots         bot.Interface
-	Debug        bool
-	IdleDuration time.Duration
-	SuperUsers   SuperUser
-	Service      service.UserService
+	TbAPI   tbAPI
+	Bots    bot.Interface
+	Service service.UserService
 
 	msgs struct {
 		once sync.Once
@@ -44,19 +35,8 @@ type tbAPI interface {
 	RestrictChatMember(config tbapi.RestrictChatMemberConfig) (tbapi.APIResponse, error)
 }
 
-type msgLogger interface {
-	Save(msg *api.Message)
-}
-
 // Do process all events, blocked call
 func (l *TelegramListener) Do(ctx context.Context) (err error) {
-
-	l.msgs.once.Do(func() {
-		l.msgs.ch = make(chan api.Response, 100)
-		if l.IdleDuration == 0 {
-			l.IdleDuration = 30 * time.Second
-		}
-	})
 
 	u := tbapi.NewUpdate(0)
 	u.Timeout = 60
@@ -103,19 +83,6 @@ func (l *TelegramListener) Do(ctx context.Context) (err error) {
 			if err := l.sendBotResponse(resp); err != nil {
 				log.Printf("[ERROR] failed to respond on update, %v", err)
 			}
-
-		case resp := <-l.msgs.ch: // publish messages from outside clients
-			//todo надо подумать надо ли нам это?
-			//if err := l.sendBotResponse(resp, 1); err != nil {
-			log.Printf("[WARN] failed to respond on rtjc event, %v %s", err, resp.Text)
-			//}
-
-		case <-time.After(l.IdleDuration): // hit bots on idle timeout
-			//todo надо подумать надо ли нам это?
-			//resp := l.Bots.OnMessage(api.Update{Message: &api.Message{Text: "idle"}})
-			//if err := l.sendBotResponse(resp, l.chatID); err != nil {
-			log.Printf("[WARN] failed to respond on idle, %v", err)
-			//}
 		}
 	}
 }
@@ -133,30 +100,13 @@ func (l *TelegramListener) sendBotResponse(resp api.TelegramMessage) error {
 	if len(resp.Chattable) > 0 {
 		for _, v := range resp.Chattable {
 			log.Printf("[INFO] bot response - %v", v)
-			r, err := l.TbAPI.Send(v)
+			_, err := l.TbAPI.Send(v)
 			if err != nil {
 				return errors.Wrapf(err, "can't send message to telegram %q", v)
 			}
-			l.saveBotMessage(&r)
 		}
 	}
 	return nil
-}
-
-// Submit message text to telegram'service group
-func (l *TelegramListener) Submit(ctx context.Context, text string, pin bool) error {
-	l.msgs.once.Do(func() { l.msgs.ch = make(chan api.Response, 100) })
-
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	case l.msgs.ch <- api.Response{Text: text, Pin: pin, Send: true, Preview: true}:
-	}
-	return nil
-}
-
-func (l *TelegramListener) saveBotMessage(msg *tbapi.Message) {
-	l.MsgLogger.Save(l.transform(msg))
 }
 
 func (l *TelegramListener) transform(msg *tbapi.Message) *api.Message {
