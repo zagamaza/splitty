9b0a9ddc8caa59e43b00bc69bc865b0b82b696d2
mazanur <almazic91@gmail.com>
2026-07-13 15:35:33 +0300

    chore(android): версия 1.3 (5) для release-сборки

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "com.zagir.splitty"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
+        versionCode = 5
         versionName = "1.3"
     }
 
